500cf047f4d517e8fd5ed811f982aacd296662a0
Erlangga28 <erlanggawahyu21@gmail.com>
2022-09-13 14:47:09 +0700

    add points

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ function main() {
   var canvas = document.getElementById("myCanvas");
   var gl = canvas.getContext("webgl");
 
-  var vertexShaderCode = "void main () {" + "}";
+  var vertices= [0.5, 0.5, 0.0, 0.0, -0.5, -0.5];
+
+ 
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  var vertexShaderCode = `
+  attribute vec2 aPosition;
+  void main () {
+    gl_PointSize = 50.0;  // adding size of point
+    gl_Position = vec4(aPosition, 0.0, 1.0);
+  }
+  `;
 
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShader, vertexShaderCode); 
@@ -11,7 +25,7 @@ function main() {
 
   var fragmentShaderCode = `
         void main () {
-          gl.PointSize = 10
+          gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
         }
   `;
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -24,9 +38,13 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.clearColor(0, 0.56, 0.48, 0.0); 
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  gl.clearColor(1.0, 0.75, 0.79, 1.0); 
 
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.point, )
+  gl.drawArrays(gl.POINT, 0, 3);
 }
