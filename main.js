cfe27a8cf82315fed130fe6705837f4c4cd38fb4
Erlangga28 <erlanggawahyu21@gmail.com>
2022-09-20 14:18:24 +0700

    add Theta

--- main.js
+++ main.js
@@ -13,9 +13,13 @@ function main() {
 
   var vertexShaderCode = `
   attribute vec2 aPosition;
+  uniform float uTheta;
   void main () {
     gl_PointSize = 30.0;  // adding size of point
-    gl_Position = vec4(aPosition, 0.0, 1.0);
+    vec2 position = vec2(aPosition);
+    position.x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+    position.y = sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+    gl_Position = vec4(position, 0.0, 1.0);
   }
   `;
 
@@ -38,13 +42,25 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  var theta = 0.0;
+  // ! all qualifire
+  var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
+
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
   gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(aPosition);
 
+  function render() {
   gl.clearColor(1.0, 0.75, 0.79, 1.0); 
-
+  theta
+  
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  theta += 0.01;
+  gl.uniform1f(uTheta, theta);
+
   gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+  }
+  setInterval(render, 1000/60);
+
 }
